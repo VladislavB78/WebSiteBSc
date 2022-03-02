adb892c36d8491db606006cb1cfff8c92649bd6e
VladislavB78 <bvi.000@mail.ru>
2022-03-02 12:12:07 +0300

    Initial commit

---
+++ src/main/java/com/example/WebSiteCoursework/WebSiteCourseworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.WebSiteCoursework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSiteCourseworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSiteCourseworkApplication.class, args);
+	}
+
+}
